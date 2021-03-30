79186f237bf9653b7aaa75e04332b0c72e0a441e
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-30 11:14:56 +0600

    from now we are going to write all js as es6 and compile it with the webpack

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(){
     return (
         <div>
-            <h1>Welcome to React SSR</h1>
+            <h1>Welcome to the very first React SSR</h1>
         </div>
     )
 }
