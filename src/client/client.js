d716210df84cc884b9c0af3d36a564c455cdc5f3
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-30 11:59:36 +0600

    Add some new code to the client for testing

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,5 @@
-console.log("Hello from the client")
+import React from 'react';
+import ReactDom from 'react-dom';
+import Home from './components/Home';
+
+ReactDom.hydrate(<Home/>, document.querySelector("#root"));
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,6 +4,9 @@ function Home(){
     return (
         <div>
             <h1>Welcome to the very first React SSR</h1>
+            <button onClick={() => console.log("You clicked me")}>Click me</button>
+            <br/>
+            <button onClick={() => console.log("you clicked to the second button")}>Click me 2</button>
         </div>
     )
 }
